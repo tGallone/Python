b47581651bd3a0cb21510fa189ea87577bece739
Tony Gallone <tony.gallone@thedatashed.co.uk>
2021-05-11 16:12:51 +0100

    cc

--- Python_VideoEditor/main.py
+++ Python_VideoEditor/main.py
@@ -1,6 +1,6 @@
 from main2 import renameFiles, mergeClips
 from yt import * 
-keyword = 'Random'
+keyword = 'Random' 
 finalVideoTitle = '51 Video Merge'
 renameFiles(".",0,keyword)
 mergeClips(keyword,finalVideoTitle)
